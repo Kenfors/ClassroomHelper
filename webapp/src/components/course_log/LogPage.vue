671cc329a8fe1126dbd45e515ff2594c67ca9dbc
Kenfors <kenfors@kth.se>
2019-09-25 11:49:54 +0200

    Added another column in log

--- webapp/src/components/course_log/LogPage.vue
+++ webapp/src/components/course_log/LogPage.vue
@@ -4,7 +4,7 @@
 <div class="col">
     <h3 class="display-4 mb-5">Loggbok</h3>
     <div class="col-12 row">
-      <div class="col-5 list-group">
+      <div class="col-6 list-group">
         <div class="text-dark text-truncate row ml-1"
         v-for="(student, index) in Students" v-bind:key="student.id">
           <div class="list-group-item list-group-item-action col-4"
@@ -12,13 +12,14 @@
           v-on:click="chooseStudent(index, student.id, student.profile.name.fullName)">
             {{student.profile.name.fullName}}
           </div>
-          <textarea disabled placeholder="Funktion ej klar" class="form-control col-8" id="" cols="2" rows="2" style="resize: none;">
-
-          </textarea>
+          <textarea disabled placeholder="Funktion ej klar" class="form-control col-8" id="" cols="2" rows="2" style="resize: none;"></textarea>
         </div>
       </div>
-      <div class="col-4 bg-info">
+      <div class="col-3 bg-info">
         <h4>Här samlas loginlägg</h4>
+      </div>
+      <div class="col-3 bg-info">
+        <h4>Här samlas rättning</h4>
       </div>
     </div>
 </div>
